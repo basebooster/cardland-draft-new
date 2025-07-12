eb13b808753032f56c033505b33fdca5a5c3e1bd
basebooster <info@cardland.se>
2025-07-12 23:44:42 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,4 @@
 
-
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
-      Welcome to Cardland Admin Panel
-    </div>
-  );
-}
-
 import React from 'react';
 
 export default function Home() {
